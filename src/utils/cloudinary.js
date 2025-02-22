93c921cc0f4bfb07e8f2ace1ff7c9e1c6c06636d
Prathamesh532 <prathameshkalekar0532@gmail.com>
2025-02-22 22:20:20 +0530

    add database connection, error handling utilities, and Cloudinary integration

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,58 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+	if (!localFilePath) {
+		console.error("uploadOnCloudinary: No file path provided");
+		return null;
+	}
+	try {
+		// Upload the file to Cloudinary
+		const response = await cloudinary.uploader.upload(localFilePath, {
+			resource_type: "auto",
+		});
+
+		console.log("File uploaded successfully:", response);
+
+		// Remove the locally stored file after a successful upload
+		if (fs.existsSync(localFilePath)) {
+			fs.unlinkSync(localFilePath);
+		}
+
+		return response;
+	} catch (error) {
+		console.error("Cloudinary upload error:", error.message);
+
+		// Remove the local file only if it exists
+		if (fs.existsSync(localFilePath)) {
+			fs.unlinkSync(localFilePath);
+		}
+
+		return null;
+	}
+};
+
+const deleteFromCloudinary = async (assestPublicId) => {
+	try {
+		if (!assestPublicId) {
+			console.error("deleteFromCloudinary: Public ID of asset Not Found");
+			return null;
+		}
+		const response = await cloudinary.uploader.destroy(assestPublicId);
+
+		console.log("File Deleted successfully:", response);
+	} catch (error) {
+		console.error("Cloudinary Delete error:", error.message);
+	}
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
